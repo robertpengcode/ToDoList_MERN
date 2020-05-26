14593f79e177b3ef6609bb88c1c79b384ddd8957
RobertPeng <jenpopeng@gmail.com>
2020-05-25 20:41:03 -0400

    prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path');
+
 
 const app = express();
 
@@ -9,10 +11,18 @@ connectDB();
 // init middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API..."));
-
 // define route
 app.use("/api/tasks", require("./routes/api/tasks"));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 8000;
 
